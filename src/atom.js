51136f833f368d4338c1b9abe99dd083689f5859
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2017-04-18 14:08:01 +1200

    Ignore result of atom/transition
    All atom/transition does is invoke a side-effect - so make that explicit

--- src/atom.js
+++ src/atom.js
@@ -5,10 +5,8 @@ import { inMemory } from './plugins';
 export default (data : Object, createStore : Function = inMemory) => {
   const watchers = [];
 
-  const transition = (next, prev) => {
+  const transition = (next, prev) =>
     watchers.forEach(watcher => watcher(prev, next));
-    return next;
-  };
 
   return {
     ...createStore(data, transition),
